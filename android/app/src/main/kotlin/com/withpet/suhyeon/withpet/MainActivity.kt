9312e0760dc8a63a492bc90c2f7355e61f8eb3a5
suhyeon <tngutnqls6909@gmail.com>
2025-09-17 23:49:57 +0900

    Feat: Stop tracking some ignore files

--- android/app/src/main/kotlin/com/withpet/suhyeon/withpet/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.withpet.suhyeon.withpet
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/presentation/viewmodels/main_view_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// BottomNavigationBar의 현재 선택된 탭 인덱스를 관리하는 Provider
-final mainScreenBottomNavProvider = StateProvider<int>((ref) {
-  // 앱 시작 시 보여줄 기본 탭 인덱스 (0 = 정보 공유 탭)
-  return 0;
-});
